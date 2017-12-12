45ff85fb275fa4558c104a5edfad32fc102e6ec0
Grigorios Kalliatakis <gkalliatakis@yahoo.gr>
2017-12-12 14:44:36 +0000

    Update WEIGHTS_PATH

--- vgg16_hybrid_places_1365.py
+++ vgg16_hybrid_places_1365.py
@@ -29,8 +29,8 @@ from keras.utils.data_utils import get_file
 from keras.utils import layer_utils
 
 
-WEIGHTS_PATH = 'https://github.com/GKalliatakis/Keras-VGG16-places365/releases/download/v0.1/vgg16-hybrid1365_weights_tf_dim_ordering_tf_kernels.h5'
-WEIGHTS_PATH_NO_TOP = 'https://github.com/GKalliatakis/Keras-VGG16-places365/releases/download/v0.1/vgg16-hybrid1365_weights_tf_dim_ordering_tf_kernels_notop.h5'
+WEIGHTS_PATH = 'https://github.com/GKalliatakis/Keras-Application-Zoo/releases/download/0.1/vgg16-hybrid1365_weights_tf_dim_ordering_tf_kernels.h5'
+WEIGHTS_PATH_NO_TOP = 'https://github.com/GKalliatakis/Keras-Application-Zoo/releases/download/0.1/vgg16-hybrid1365_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
 def VGG16_Hubrid_1365(include_top=True, weights='places',
